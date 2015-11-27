2a500a1b453f0aad63ad6ab7f67a4b6035327350
Tugce mini <vayen01@vayenMini.local>
2015-11-27 15:39:22 +0200

    Initial commit.

---
+++ app/src/androidTest/java/com/example/vayen01/masterslave/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.vayen01.masterslave;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
